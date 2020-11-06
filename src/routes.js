fddfd4bc9bcb51a3e979b65acd3e1a047613b048
Rogério Feliciano dos Santos <felicianoi9@gmail.com>
2020-11-06 16:23:51 -0300

    Single image file upload is done

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+
+
+routes.post('/posts', multer(multerConfig).single('file'), (req,resp) => {
+    console.log(req.file);
+    
+    return resp.json({ hello: "People" });
+});
+
+module.exports = routes;
